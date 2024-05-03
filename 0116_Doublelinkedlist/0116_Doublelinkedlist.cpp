db90608911d4926848274ec335162d9b4f1d6ab2
ZahwaAnritia <anritia15@gmail.com>
2024-05-03 18:50:07 +0700

    add  node baru di antara node yang ada berdasarkan nomor roll

--- 0116_Doublelinkedlist/0116_Doublelinkedlist.cpp
+++ 0116_Doublelinkedlist/0116_Doublelinkedlist.cpp
@@ -36,6 +36,14 @@ void addNode() {
 	else {
 		Node* current = START;
 		Node* previous = NULL;
+
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{
+			previous = current;
+			current = current->next;
+		}
+		newNode->next = current;
+		newNode->prev = previous;
 	}
 }
 
