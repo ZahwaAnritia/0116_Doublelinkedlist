179ac95b0fab9e559bfc36d6ff08de4b41906365
ZahwaAnritia <anritia15@gmail.com>
2024-05-03 19:40:13 +0700

    add proses untuk mencari dan menampilkan data mahasiswa

--- 0116_Doublelinkedlist/0116_Doublelinkedlist.cpp
+++ 0116_Doublelinkedlist/0116_Doublelinkedlist.cpp
@@ -176,7 +176,16 @@ void searchData()
 	prev = curr = NULL;
 	cout << "\nEnter the roll number of the student whose record you want to search: ";
 
-
+	int num;
+	cin >> num;
+	if (search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else
+	{
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
 }
 int main() 
 {
